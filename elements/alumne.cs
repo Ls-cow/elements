712cc842772bf9fd267aeb712828ef589aa46fb7
Sanchez Laura <a2207820@institutmontilivi.cat>
2023-03-08 12:03:50 +0100

    primer commit

---
+++ elements/alumne.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace elements
+{
+    internal class alumne
+    {
+        private string cognom;
+        private DateFormat dataNaixament;
+        private string email;
+        private string nif;
+        private string nom;
+        private int numAlumne;
+        private char sexe;
+        private int telefon;
+
+    }
+}
