c054b6fbe5df01b911d8c46a631b7f3e9281effe
Sanchez Laura <a2207820@institutmontilivi.cat>
2023-03-09 09:21:52 +0100

    Segunda modificación con casi todos los metodos creados, aún no tinene contenido pero es para seguir la estructura

--- elements/alumne.cs
+++ elements/alumne.cs
@@ -10,7 +10,7 @@ namespace elements
     internal class alumne
     {
         private string cognom;
-        private DateFormat dataNaixament;
+        private DateTime dataNaixament;
         private string email;
         private string nif;
         private string nom;
@@ -18,5 +18,70 @@ namespace elements
         private char sexe;
         private int telefon;
 
-    }
+        public string Cognom
+        {
+            get { return cognom; }
+            set { cognom = value; }
+        }
+        public string DataNaixament
+        {
+            get { return dataNaixament; }
+            set { dataNaixament = value; }
+        }
+        public string Email 
+        {
+            get { return  email; }
+            set { email = value; }
+        }
+        public string Nif
+        {
+            get { return nif; }
+            set { nif = value; }
+        }
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value;}
+        }
+        public char Sexe
+        {
+            get { return sexe; }
+            set { sexe = value; }
+        }
+        public int Telefon
+        {
+            get { return telefon; }
+            set { telefon = value; }
+        }
+        private bool DataValid (DateTime data)
+        {
+
+        }
+        public int Edat (string data)
+        {
+
+        }
+        private string EmailValid (string correo)
+        { 
+        }
+        public string Mostra  (string objecte)
+        {
+
+        }
+        private bool NifValid (string document)
+        {
+
+        }
+        private string NomValid (string nom)
+        {
+
+        }
+        public void NumAlumne (int numero)
+        {
+
+        }
+        private int TelfValid (int tlf)
+        { 
+        }
+        public
 }
