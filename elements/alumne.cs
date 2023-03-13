7bc4f5c4f88468060d246bac523c94acd58e6f17
Ls-cow <124570503+Ls-cow@users.noreply.github.com>
2023-03-13 11:28:56 +0100

    actualizacion de metodos

--- elements/alumne.cs
+++ elements/alumne.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualBasic;
 using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -9,187 +10,174 @@ using System.Threading.Tasks;
 
 namespace elements
 {
-<<<<<<< HEAD
     internal class alumne
     { //ATRIBUTS
-=======
-    internal class Alumne
-    {
->>>>>>> 77ef636390b2283cf5588be1a83b14a41e0b7398
-        private string cognom;
-        private DateTime dataNaixament;
-        private string email;
-        private string nif;
-        private string nom;
-        private int numAlumne;
-        private char sexe;
-        private int telefon;
 
-<<<<<<< HEAD
-        //PROPIETATS
-=======
-        #region//Propietats
->>>>>>> 77ef636390b2283cf5588be1a83b14a41e0b7398
-        public string Cognom
+        internal class Alumne
         {
-            get { return cognom; }
-            set { cognom = value; }
-        }
-        public DateTime DataNaixament
-        {
-<<<<<<< HEAD
-            get { return dataNaixament.ToString; }
-            set { DateTime.TryParse(value, dataNaixament); }
-=======
-            get { return dataNaixament; }
-            set { dataNaixament = value; }
->>>>>>> 77ef636390b2283cf5588be1a83b14a41e0b7398
-        }
-        public string DataNaix
-        {
-            get { return dataNaixament.ToString(); }
-            set { DateTime.TryParse(value, out dataNaixament); }
-        }
-        public string Email
-        {
-            get { return email; }
-            set { email = value; }
-        }
-        public string Nif
-        {
-            get { return nif; }
-            set { nif = value; }
-        }
-        public string Nom
-        {
-            get { return nom; }
-            set { nom = value; }
-        }
-        public char Sexe
-        {
-            get { return sexe; }
-            set { sexe = value; }
-        }
-        public int Telefon
-        {
-            get { return telefon; }
-            set { telefon = value; }
-        }
-<<<<<<< HEAD
-        //METODES
-        private bool DataValid (DateTime data)
-=======
-        #endregion
-        public bool DataValid(DateTime data)
->>>>>>> 77ef636390b2283cf5588be1a83b14a41e0b7398
-        {
-            DateTime limit = new DateTime(1900, 01, 01);
-            bool valid = false;
-            do
+            private string cognom;
+            private DateTime dataNaixament;
+            private string email;
+            private string nif;
+            private string nom;
+            private int numAlumne;
+            private char sexe;
+            private int telefon;
+
+
+            //PROPIETATS
+
+            #region//Propietats
+            public string Cognom
             {
-                if (data > DateTime.Now || data < limit)
+                get { return cognom; }
+                set { cognom = value; }
+            }
+            public DateTime DataNaixament
+            {
+                get { return dataNaixament.ToString; }
+                set { DateTime.TryParse(value, dataNaixament); }
+
+                get { return dataNaixament; }
+                set { dataNaixament = value; }
+            }
+            public string DataNaix
+            {
+                get { return dataNaixament.ToString(); }
+                set { DateTime.TryParse(value, out dataNaixament); }
+            }
+            public string Email
+            {
+                get { return email; }
+                set { email = value; }
+            }
+            public string Nif
+            {
+                get { return nif; }
+                set { nif = value; }
+            }
+            public string Nom
+            {
+                get { return nom; }
+                set { nom = value; }
+            }
+            public char Sexe
+            {
+                get { return sexe; }
+                set { sexe = value; }
+            }
+            public int Telefon
+            {
+                get { return telefon; }
+                set { telefon = value; }
+            }
+            //METODES
+            #endregion
+            public bool DataValid(DateTime data)
+            {
+                DateTime limit = new DateTime(1900, 01, 01);
+                bool valid = false;
+                do
                 {
-                    Console.WriteLine("Data incorrecte, torna a introduirla: ");
-                    data = DateTime.Parse(Console.ReadLine());
-                }
-                else
-                    valid = true;
-            } while (!valid);
-            return valid;
-        }
-        public int Edat()
-        {
-            int edat = DateTime.Now.Year - dataNaixament.Year;
-            if (DateTime.Now.Month < dataNaixament.Month)
-                edat--;
-            else
+                    if (data > DateTime.Now || data < limit)
+                    {
+                        Console.WriteLine("Data incorrecte, torna a introduirla: ");
+                        data = DateTime.Parse(Console.ReadLine());
+                    }
+                    else
+                        valid = true;
+                } while (!valid);
+                return valid;
+            }
+            public int Edat()
             {
-                if (DateTime.Now.Day < dataNaixament.Day)
+                int edat = DateTime.Now.Year - dataNaixament.Year;
+                if (DateTime.Now.Month < dataNaixament.Month)
                     edat--;
+                else
+                {
+                    if (DateTime.Now.Day < dataNaixament.Day)
+                        edat--;
+                }
+                return edat;
             }
-            return edat;
-        }
-        private string EmailValid(string correo)
-        {
-            string patron = "[^@]+@[^\\.]+\\..+";
-            Regex regex = new Regex(patron);
-            do
+            private string EmailValid(string correo)
             {
-                Console.WriteLine("Error,escribe nuevamente");
-                correo = Console.ReadLine();
+                string patron = "[^@]+@[^\\.]+\\..+";
+                Regex regex = new Regex(patron);
+                do
+                {
+                    Console.WriteLine("Error,escribe nuevamente");
+                    correo = Console.ReadLine();
+                }
+                while (!regex.IsMatch(email));
+                return correo.ToLower();
             }
-            while (!regex.IsMatch(email));
-            return correo.ToLower();
-        }
-        public void Mostra()
-        {
-            Console.WriteLine($"DADES ALUMNE {nom}");
-            Console.WriteLine("---------------------");
-            Console.WriteLine($"Cognom: {cognom}");
-            Console.WriteLine($"NIF: {nif}\tNum Alumne: {numAlumne}");
-            Console.WriteLine($"Telf: {telefon}\tE-mail: {email}");
-            Console.WriteLine($"Edat: {this.Edat()}");
-            Console.WriteLine($"Sexe: {sexe}");
-        }
-        private string NifValid(string document)
-        {
-            string dniLetra, dniNumeros;
-            string[] control = { "T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E" };
-            while (document.Length != 9)
+            public void Mostra()
             {
-                Console.WriteLine("Dni mal escrito, ingrese nuevamente.");
-                document = Console.ReadLine();
+                Console.WriteLine($"DADES ALUMNE {nom}");
+                Console.WriteLine("---------------------");
+                Console.WriteLine($"Cognom: {cognom}");
+                Console.WriteLine($"NIF: {nif}\tNum Alumne: {numAlumne}");
+                Console.WriteLine($"Telf: {telefon}\tE-mail: {email}");
+                Console.WriteLine($"Edat: {this.Edat()}");
+                Console.WriteLine($"Sexe: {sexe}");
             }
-            dniNumeros = document.Substring(0, document.Length - 1);
-            dniLetra = document.Substring(document.Length - 1, 1);
-            int numeros = Int32.Parse(dniNumeros);
-            var mod = numeros % 23;
-            document = dniNumeros + control[mod];
-            return document;
-        }
-        private string NomValid(string nom)
-        {
-            Regex validar = new Regex("[a-zA-Z]");
-            if(!validar.IsMatch(nom))
+            private string NifValid(string document)
             {
-                Console.WriteLine("El només pot contenir lletres, torna a escriure");
-                nom = Console.ReadLine();
+                string dniLetra, dniNumeros;
+                string[] control = { "T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E" };
+                while (document.Length != 9)
+                {
+                    Console.WriteLine("Dni mal escrito, ingrese nuevamente.");
+                    document = Console.ReadLine();
+                }
+                dniNumeros = document.Substring(0, document.Length - 1);
+                dniLetra = document.Substring(document.Length - 1, 1);
+                int numeros = Int32.Parse(dniNumeros);
+                var mod = numeros % 23;
+                document = dniNumeros + control[mod];
+                return document;
             }
-            return nom;
+            private string NomValid(string nom)
+            {
+                Regex validar = new Regex("[a-zA-Z]");
+                if (!validar.IsMatch(nom))
+                {
+                    Console.WriteLine("El només pot contenir lletres, torna a escriure");
+                    nom = Console.ReadLine();
+                }
+                return nom;
 
-        }
-<<<<<<< HEAD
-        public int NumAlumne(string[] alumne )
-=======
-        public void NumAlumne(int numero)
->>>>>>> 77ef636390b2283cf5588be1a83b14a41e0b7398
-        {
-            return alumne.Length + 1;
-        }
-        private string TelfValid(string tlf)
-        {
-            int i = 0, numeroInt;
-            while (tlf.Length != 9)
-            {
-                Console.WriteLine("Numero mal escrito, ingrese de nuevo.");
-                tlf = Console.ReadLine();
             }
-            bool esNumero = int.TryParse(tlf, out numeroInt);
-            while (esNumero == false)
+            public int NumAlumne()
             {
-                Console.WriteLine("El numero no puede contener letras, ingrese de nuevo.");
-                tlf = Console.ReadLine();
-                esNumero = int.TryParse(tlf, out numeroInt);
+
             }
-            return tlf;
+            private string TelfValid(string tlf)
+            {
+                int i = 0, numeroInt;
+                while (tlf.Length != 9)
+                {
+                    Console.WriteLine("Numero mal escrito, ingrese de nuevo.");
+                    tlf = Console.ReadLine();
+                }
+                bool esNumero = int.TryParse(tlf, out numeroInt);
+                while (esNumero == false)
+                {
+                    Console.WriteLine("El numero no puede contener letras, ingrese de nuevo.");
+                    tlf = Console.ReadLine();
+                    esNumero = int.TryParse(tlf, out numeroInt);
+                }
+                return tlf;
+            }
+            public string Tostrinng()
+            {
+                return $"Nom: {nom} \rCognom: {cognom}\rNum alumne: {numAlumne} \rData de naixament: {dataNaixament}\rNIF:{nif}\rEmail: {email}\r" +
+                    $"Telefon: {telefon}\r Sexe:{sexe}";
+            }
+           
         }
-<<<<<<< HEAD
-        public string Tostrinng() 
-        {
-            return $"Nom: {nom} \rCognom: {cognom}\rNum alumne: {numAlumne} \rData de naixament: {dataNaixament}\rNIF:{nif}\rEmail: {email}\r" +
-                $"Telefon: {telefon}\r Sexe:{sexe}";
-        }
-=======
     }
->>>>>>> 77ef636390b2283cf5588be1a83b14a41e0b7398
 }
+
